e3903aa4ad15dd111817c8b73d9244df4fd0f0aa
dekonunes <dekonunesss@gmail.com>
2015-11-03 02:41:12 -0200

    Encaminhando

--- main.c
+++ main.c
@@ -20,19 +20,25 @@ int main(int argc, char **argv) {
 	unsigned int tampag = argv[1]; //tamanho da página;
 	unsigned int memfisica = argv[2]; //tamanho da memória fisica
 	unsigned int memlogica = argv[3]; //tamanho da memória lógica
-	int numeroendereco = 0, faltapagina = 0,enderecos[100],i = 0;
+	int numeroendereco = 0, faltapagina = 0,i = 0;
 	float taxa = 0.0;
+	unsigned int qtdPagFisica = ((memfisica*1000)/tampag), qtdPagLogica = ((memlogica*1000)/tampag);
+	int *enderecos,*vetorFisico,vetorLogico;
+
+	vetorLogico = (int*) malloc(sizeof(int)*qtdPagLogica)
 
 	FILE *file;
 	file = fopen("ps1.in", "r");
-
 
 	fscanf (file, "%d", &enderecos[i]);
 	while (!feof (file))  {
 	  printf ("%d \n", enderecos[i]);
 	  fscanf (file, "%d", &enderecos[i]);
+	  numeroendereco++;
 	}
 	fclose(file);
+	i = 0;
+	//while (enderecos[i] > 0) {	}
 
 
 	printf("%lu enderecos lidos, %lu faltas de pagina (taxa=%.1f%%)\n",numeroendereco,faltapagina,taxa);
