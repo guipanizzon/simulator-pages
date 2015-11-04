efe8c3e4d782671cefb4d73c5f17e61fca2e9ef7
dekonunes <dekonunesss@gmail.com>
2015-11-04 08:23:43 -0200

    quase

--- main.c
+++ main.c
@@ -20,39 +20,66 @@ int main(int argc, char **argv) {
 	unsigned int tampag = atoi(argv[1]); //tamanho da página;
 	unsigned int memfisica = atoi(argv[2]); //tamanho da memória fisica
 	unsigned int memlogica = atoi(argv[3]); //tamanho da memória lógica
-	int numeroendereco = 0, faltapagina = 0,i = 0;
+	int i = 0, numeroDaPagLogica = 0, flag = 0;
 	float taxa = 0.0;
-	unsigned int qtdPagFisica = ((memfisica*1000)/tampag), qtdPagLogica = ((memlogica*1000)/tampag);
-	unsigned int *enderecos,*vetorFisico,*vetorLogico;
+	long unsigned int faltapagina = 0, numeroendereco = 0, qtdPagFisica =
+			((memfisica * 1000) / tampag), qtdPagLogica = ((memlogica * 1000)
+			/ tampag);
+	int *enderecos, *vetorFisico, *vetorLogico;
 
-	vetorLogico = (int*) malloc(sizeof(unsigned int)*qtdPagLogica);
-	vetorFisico = (int*) malloc(sizeof(int)*qtdPagFisica);
+	vetorLogico = (int*) malloc(sizeof(unsigned int) * qtdPagLogica);
+	vetorFisico = (int*) malloc(sizeof(int) * qtdPagFisica);
 
+	for (int i = 0; i < qtdPagFisica; ++i) {
+		vetorFisico[i] = -1;
+	}
 
 	FILE *file;
 	file = fopen("ps1.in", "r");
 
-	while (!feof(file))  {
-		fscanf (file, "%d", &i);
-	  numeroendereco++;
+	while (!feof(file)) {
+		fscanf(file, "%d", &i);
+		numeroendereco++;
 	}
-	printf("qtd end %d\n",numeroendereco);
 
 	rewind(file);
 
-	enderecos = (int*) malloc(sizeof(int)*numeroendereco);
+	enderecos = (int*) malloc(sizeof(int) * numeroendereco);
 	numeroendereco = 0;
-	fscanf (file, "%d", enderecos[numeroendereco]);
-	while (!feof (file))  {
-	  printf ("%d \n", enderecos[numeroendereco]);
-	  fscanf (file, "%d", &enderecos[numeroendereco]);
-	  numeroendereco++;
+	fscanf(file, "%d", &enderecos[numeroendereco]);
+	while (!feof(file)) {
+		printf("%d \n", enderecos[numeroendereco]);
+		numeroDaPagLogica = enderecos[numeroendereco] / tampag;
+		printf("Numero da pagina logica = %d\n", numeroDaPagLogica);
+		for (int j = 0; j < qtdPagFisica; ++j) {
+			for (int i = 0; i < qtdPagFisica; ++i) {
+				printf("vetor fisico:%d\n", vetorFisico[i]);
+				if (vetorFisico[j] == numeroDaPagLogica) {
+					printf("Posição na mem Fisica\n	");
+					flag = 1;
+				}
+			}
+			if (flag == 0) {
+				faltapagina++;
+				while (vetorFisico[j] != -1) {
+					j++;
+				}
+				if (j > qtdPagFisica) {
+					/*Aqui entra o MRU*/
+					j = 0;
+				}
+
+				vetorFisico[j] = numeroDaPagLogica;
+				break;
+			}
+		}
+		flag = 0;
+		numeroendereco++;
+		fscanf(file, "%d", &enderecos[numeroendereco]);
 	}
 	fclose(file);
-	i = 0;
-	//while (enderecos[i] > 0) {	}
 
-
-	//printf("%lu enderecos lidos, %lu faltas de pagina (taxa=%.1f%%)\n",numeroendereco,faltapagina,taxa);
+	printf("%lu enderecos lidos, %lu faltas de pagina (taxa=%.1f%%)\n",
+			numeroendereco, faltapagina, taxa);
 	return EXIT_SUCCESS;
 }
